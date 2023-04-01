03757bdb7d86e4cb295382aebfb4d824509258ee
Carlos Mazzon <carlosmazzon@hotmail.com>
2023-04-01 15:44:32 -0300

    update useEffect

--- src/Screens/Home/Home.tsx
+++ src/Screens/Home/Home.tsx
@@ -5,32 +5,32 @@ import {
   TextField,
   Typography,
   Box,
-} from "@mui/material";
-import { useState, useEffect } from "react";
-import CardView from "../../Components/CardView";
-import { IMovie } from "../../Interfaces/IMovies";
-import useAPI from "../../Services/APIs/Common/useAPI";
-import Movies from "../../Services/APIs/Movies/Movies";
-import Colors from "../../Utils/Common/Colors";
-import CircularProgress from "@mui/material/CircularProgress";
-import { Else, If, Then } from "react-if";
-import "./HomeStyles.css";
+} from '@mui/material';
+import { useState, useEffect } from 'react';
+import CardView from '../../Components/CardView';
+import { IMovie } from '../../Interfaces/IMovies';
+import useAPI from '../../Services/APIs/Common/useAPI';
+import Movies from '../../Services/APIs/Movies/Movies';
+import Colors from '../../Utils/Common/Colors';
+import CircularProgress from '@mui/material/CircularProgress';
+import { Else, If, Then } from 'react-if';
+import './HomeStyles.css';
 
 const Home = () => {
   const [movies, setMovies] = useState<IMovie[]>([]);
-  const [searchText, setSearchText] = useState<string>("");
-  const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [searchText, setSearchText] = useState<string>('');
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   const getPersonAPI = useAPI(Movies.getMovies);
 
   useEffect(() => {
-    getData()
-  }, []);
+    getData();
+  });
 
   const getData = () => {
     setIsLoading(true);
     getPersonAPI
       .requestPromise(searchText)
-      .then((info: IMovie[]) => {        
+      .then((info: IMovie[]) => {
         console.log(info);
         setMovies(info);
         setIsLoading(false);
@@ -46,7 +46,6 @@ const Home = () => {
     setSearchText(event.target.value);
   };
 
-
   let arrayInfo: JSX.Element[] = [];
   movies.forEach((movie: IMovie) => {
     arrayInfo.push(
@@ -57,30 +56,33 @@ const Home = () => {
   });
 
   return (
-    <Container maxWidth="lg">
-      <Grid container direction="row" spacing={15}>
+    <Container maxWidth='lg'>
+      <Grid container direction='row' spacing={15}>
         <Grid item lg={12}>
-          <Grid container spacing={2} className="header">
+          <Grid container spacing={2} className='header'>
             <Grid item lg={6}>
-              <Typography variant="h1" color={Colors.PrimaryLight}>
+              <Typography variant='h1' color={Colors.PrimaryLight}>
                 Movies Catalog
               </Typography>
             </Grid>
-            <Grid item lg={6} className="buttonsDiv">
+            <Grid item lg={6} className='buttonsDiv'>
               <TextField
-                id="filled-basic"
-                label="Pesquisar"
-                size="small"
+                id='filled-basic'
+                label='Pesquisar'
+                size='small'
                 onChange={onChangeSearch}
-              /> &nbsp;
-              <Button variant="contained" onClick={() => getData()}>Buscar</Button>
+              />{' '}
+              &nbsp;
+              <Button variant='contained' onClick={() => getData()}>
+                Buscar
+              </Button>
             </Grid>
           </Grid>
         </Grid>
-        <Grid item lg={12} className="divCards">
+        <Grid item lg={12} className='divCards'>
           <If condition={isLoading}>
             <Then>
-              <Box className="progress">
+              <Box className='progress'>
                 <CircularProgress />
               </Box>
             </Then>
